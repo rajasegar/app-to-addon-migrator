2f92226feeadff8aea3fa249d6fc24c5580b0280
Vinodh Kumar <vinodhkumar.manickam@freshworks.com>
2020-06-24 15:48:27 +0530

    Fixed layout property bug

--- transforms/add-layout-property/__testfixtures__/basic.output.js
+++ transforms/add-layout-property/__testfixtures__/basic.output.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import { run } from '@ember/runloop';
 import { set } from '@ember/object';
-import { inject as service } from '@ember/service';
 
 import layout from '../../templates/components/file-name';
 
